565a60d3b5bd887da3a15c7d3204641832fb15e0
astnrhy30 <astia41083.pesonaxxi@gmail.com>
2021-05-13 11:49:48 +0700

    Menambah modal, merapikan tampilan index  dan profilUTM.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log("astia")
+var modal = document.getElementById("modalForm");
+var btn = document.getElementById("buatCV");
+var close_modal = document.getElementsByClassName("closeMdal")[0];
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
